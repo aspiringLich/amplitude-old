8162059298afe43909e80ae5bee563fbfb2fcbf0
Brandon Li <sirbrandonthenerd@gmail.com>
2023-05-31 16:59:49 -0400

    tailwind css works

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+    import "../app.postcss";
+
+    import "@skeletonlabs/skeleton/themes/theme-skeleton.css";
+    import "@skeletonlabs/skeleton/styles/skeleton.css";
+    import "../app.postcss";
 </script>
 
 <slot />
 
 <style lang="scss" global>
-    @import "$styles/global";
+    // @import "$styles/global";
     @import "$styles/styles";
-    @import "$styles/variables";
+    // @import "$styles/variables";
 </style>
--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,56 +1,60 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-import sveltePreprocess from 'svelte-preprocess';
-import path from 'path';
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/kit/vite";
+import sveltePreprocess from "svelte-preprocess";
+import path from "path";
 
 function importer(url) {
-	for (const [alias, aliasPath] in
-		[
-			["$lib", path.resolve("./src/lib")],
-			["$src", path.resolve("./src")],
-			["$cmpt", path.resolve("./src/components")],
-			["$styles", path.resolve("./src/styles")],
-		]) {
-		if (url.startsWith(alias)) {
-			return {
-				file: url.replace(alias, aliasPath),
-			};
-		}
-	}
+  for (const [alias, aliasPath] in [
+    ["$lib", path.resolve("./src/lib")],
+    ["$src", path.resolve("./src")],
+    ["$cmpt", path.resolve("./src/components")],
+    ["$styles", path.resolve("./src/styles")],
+  ]) {
+    if (url.startsWith(alias)) {
+      return {
+        file: url.replace(alias, aliasPath),
+      };
+    }
+  }
 }
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [
-		vitePreprocess({
-			style: {
-				css: {
-					preprocessorOptions: {
-						scss: {
-							importer,
-						}
-					}
-				}
-			}
-		}), 
-		sveltePreprocess({
-			scss: {
-				importer,
-			}
-		})
-	],
-	// experimental: {
-	// 	useVitePreprocess: true,
-	// },
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: [
+    vitePreprocess({
+      style: {
+        css: {
+          preprocessorOptions: {
+            scss: {
+              importer,
+            },
+            postcss: {
+              importer
+            }
+          },
+        },
+      },
+    }),
+    // sveltePreprocess({
+    //   scss: {
+    //     importer,
+    //   },
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-	},
+    //   postcss: true,
+    // }),
+  ],
+  // experimental: {
+  // 	useVitePreprocess: true,
+  // },
+
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter(),
+  },
 };
 
 export default config;
