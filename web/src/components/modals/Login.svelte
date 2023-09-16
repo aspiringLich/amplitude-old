41581e57ee8d486ec82a0ea20eedba53e1be067c
Connor Slade <connor@connorcode.com>
2023-09-16 16:02:06 -0400

    Save old page when logging in

--- web/src/components/modals/Login.svelte
+++ web/src/components/modals/Login.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     import Modal from "$cmpt/modals/Modal.svelte";
     import { supportedPlatforms } from "$lib/fetch";
+
+    const redirect = (platform) => (document.location = `${platform.path}?r=${encodeURIComponent(document.location.pathname)}`);
 </script>
 
 <Modal title="Login">
     <div class="flex flex-col gap-3 m-auto w-80">
         {#each supportedPlatforms as provider}
-            <button on:click={() => (document.location = provider.path)} class="btn variant-outline-surface">
+            <button on:click={() => redirect(provider)} class="btn variant-outline-surface">
                 Login with {provider.name}
             </button>
         {/each}
